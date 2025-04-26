7f6cd2e114144cd0f54c28f8cc952403719c40a1
Stepawka <stepan-lvov-04@mail.ru>
2025-04-26 23:16:44 +0300

    fix: Обёртка функций модалки в useCallback

--- client/src/components/modal/modal.tsx
+++ client/src/components/modal/modal.tsx
@@ -1,29 +1,33 @@
 import { ModalUI } from "@ui/modal";
-import { FC, memo, MouseEventHandler, useEffect } from "react";
+import { FC, memo, MouseEventHandler, useCallback, useEffect } from "react";
 import { createPortal } from "react-dom";
 import { TModalProps } from "./type";
 
 const modalRoot = document.getElementById("modals");
 
 export const Modal: FC<TModalProps> = memo(({ isOpen, children, onClose }) => {
-  const closeByEsc = (evt: KeyboardEvent) => {
-    if (evt.key === "Escape") {
-      onClose();
-    }
-  };
+  const closeByEsc = useCallback(
+    (evt: KeyboardEvent) => {
+      if (evt.key === "Escape") {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
-  const handleOverlayClick: MouseEventHandler = (evt) => {
-    if (evt.target === evt.currentTarget) {
-      onClose();
-    }
-  };
+  const handleOverlayClick: MouseEventHandler = useCallback(
+    (evt) => {
+      if (evt.target === evt.currentTarget) {
+        onClose();
+      }
+    },
+    [onClose]
+  );
 
   useEffect(() => {
     document.addEventListener("keydown", closeByEsc);
     return () => document.removeEventListener("keydown", closeByEsc);
-  }, []);
-
-  console.log(isOpen);
+  }, [closeByEsc]);
 
   return createPortal(
     <ModalUI
