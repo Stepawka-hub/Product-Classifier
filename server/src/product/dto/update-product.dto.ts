27e05d596a377481aedbdee2ad9f933d7edec101
Stepawka <stepan-lvov-04@mail.ru>
2025-04-28 01:37:31 +0300

    feat: Обновление изделия

--- server/src/product/dto/update-product.dto.ts
+++ server/src/product/dto/update-product.dto.ts
@@ -1,4 +1,6 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateProductDto } from './create-product.dto';
-
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+  id: number;
+  name: string;
+  parentName: string;
+  unitName: string;
+}
--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -5,10 +5,17 @@ import { ProductRepository } from 'src/product/repositories/product.repository';
 import { PaginatedResponseDto } from 'src/common/dto/paginated.dto';
 import { BaseResponseDto } from 'src/common/dto/response.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { getErrorMessage } from 'src/utils/error-handler';
+import { CategoryRepository } from 'src/category/repositores/category.repository';
+import { UnitRepository } from 'src/unit/repositories/unit.repository';
 
 @Injectable()
 export class ProductService {
-  constructor(private productRepository: ProductRepository) {}
+  constructor(
+    private productRepository: ProductRepository,
+    private categoryRepository: CategoryRepository,
+    private unitRepository: UnitRepository,
+  ) {}
 
   async findAllWithPagination(
     page: number = 1,
@@ -34,7 +41,47 @@ export class ProductService {
   }
 
   async updateProduct(dto: UpdateProductDto): Promise<BaseResponseDto> {
-    return await this.productRepository.updateProduct(dto);
+    try {
+      const { id, name, parentName, unitName } = dto;
+      const isProductExist = await this.productRepository.findOne({
+        where: { name },
+      });
+
+      if (isProductExist) {
+        return BaseResponseDto.Error(
+          'Изделие с таким названием уже существует!',
+        );
+      }
+
+      const category = await this.categoryRepository.findOne({
+        where: { name: parentName },
+      });
+      if (!category) {
+        return BaseResponseDto.Error('Указанной категории не существует!');
+      }
+
+      const unit = await this.unitRepository.findOne({
+        where: { name: unitName },
+      });
+      if (!unit) {
+        return BaseResponseDto.Error('Указанной ЕИ не существует!');
+      }
+
+      const updateResult = await this.productRepository.update(
+        { id },
+        { name, parent: category, unit },
+      );
+
+      if (updateResult.affected === 0) {
+        return BaseResponseDto.Error('Запись не найдена или не была изменена!');
+      }
+
+      return BaseResponseDto.Success();
+    } catch (error) {
+      return BaseResponseDto.Error(
+        'Ошибка при обновлении записи: ' + getErrorMessage(error),
+      );
+    }
   }
 
   async deleteProduct(id: number): Promise<BaseResponseDto> {
